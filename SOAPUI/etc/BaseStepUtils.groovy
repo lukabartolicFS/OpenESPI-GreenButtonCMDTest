49bfe2a096c95e92158c3c6bcf859d6db607945d
RonPasquarelli <rpasquarelli@gmail.com>
2014-01-31 13:49:44 -0500

    additional cuccumber implementations and debugging log step utilities

--- SOAPUI/etc/BaseStepUtils.groovy
+++ SOAPUI/etc/BaseStepUtils.groovy
@@ -25,13 +25,28 @@ public class BaseStepUtilsA {
 	public static final String CUSTODIAN_USERNAME = "grace";
 	public static final String CUSTODIAN_PASSWORD = "koala";
 
+	private static boolean _bLogActions = true;
+
 	public static quit()
 	{
 		_driver.quit();
 	}
 
+	private static void LogAction()
+	{
+	    if(_bLogActions)
+        {
+        	if(Thread.currentThread().getStackTrace()[8].getLineNumber()!=-1)
+        	{
+        		log.info("Action from script line[" + Thread.currentThread().getStackTrace()[8].getLineNumber() + "]:" + Thread.currentThread().getStackTrace()[3].getMethodName())
+    		}
+    		//log.info("Stack:" + Thread.currentThread().getStackTrace());
+        }	
+	}
+
 	public static void Before()
 	{	
+		LogAction();
         _driver.get(THIRD_PARTY_BASE_URL + "/j_spring_security_logout");
         _driver.get(DATA_CUSTODIAN_BASE_URL + "/logout.do");
         login(DATA_CUSTODIAN_BASE_URL, "grace", "koala");    
@@ -41,6 +56,12 @@ public class BaseStepUtilsA {
 
 	public static void login(String strEndpoint,String strUserName,String strPassword)
 	{
+        //navigateTo(context, "/");
+        //if (_driver.findElements(By.id("logout")).size() > 0) {
+        //    logout();
+        //}
+        //logout(); 
+        LogAction();
 		_driver.get(strEndpoint);
 		_driver.findElement(By.id("login")).click();
 		_driver.findElement(By.name("j_username")).clear();
@@ -50,57 +71,83 @@ public class BaseStepUtilsA {
 		_driver.findElement(By.name("submit")).click();
 	}
 
-	public static void logout()
+	public int GetNumElsByTagName(String strTagName)
 	{
-		_driver.findElement(By.id("logout")).click();
+		LogAction();
+
+		return _driver.findElements(By.tagName(strTagName)).size();
 	}
+
+	public static void logout(String strContext)
+	{
+		LogAction();
+	    _driver.get(strContext);
+        if (_driver.findElements(By.id("logout")).size() > 0) {
+            _driver.findElement(By.id("logout")).click();
+        }	
+
+		//_driver.findElement(By.id("logout")).click();
+	}
+
+    public static void navigateTo(String context, String path) 
+    {
+    	LogAction();
+        _driver.get(BASE_URL + context + path);
+    }
 
 	public static void sendKeysByName(String strLink,String strValue)
 	{
+		LogAction();
 		_driver.findElement(By.name(strLink)).sendKeys(strValue);
 	}
 	
 	public static void clickByXpath(String strLink)
 	{
+		LogAction();
 		_driver.findElement(By.xpath(strLink)).click();
 	}
 
 	public static void clickLinkByText(String strLink)
 	{
-        WebElement link = _driver.findElement(By.linkText(strLink));
-        link.click();		
+		LogAction();
+        _driver.findElement(By.linkText(strLink)).click();	
 	}
 
 	public static clickByClass(String strLink)
 	{
-        WebElement radio = _driver.findElement(By.className(strLink));
-        radio.click();
+		LogAction();
+        _driver.findElement(By.className(strLink)).click();
 	}
 
 	public static clickBypartialLinkText(String strLink)
 	{
+		LogAction();
 		_driver.findElement(By.partialLinkText(strLink)).click();	
 	}
 
 	public static fillInById(String strName,String strValue)
 	{
+		LogAction();
 		_driver.findElement(By.id(strName)).clear();
 		_driver.findElement(By.id(strName)).sendKeys(strValue);
 	}
 
 	public static fillInByName(String strName,String strValue)
 	{
+		LogAction();
 		_driver.findElement(By.name(strName)).clear();
 		_driver.findElement(By.name(strName)).sendKeys(strValue);
 	}
 
 	public static clickByName(String strName)
 	{
+		LogAction();
 		_driver.findElement(By.name(strName)).click();
 	}
 
 	public static assertUrlContains(String strValue)
 	{
+		LogAction();
 		if(!_driver.getCurrentUrl().contains(strValue))
 		{
 			log.error("Current URL does not contain: '" + strValue + "'");
@@ -111,6 +158,7 @@ public class BaseStepUtilsA {
 
 	public static assertUrlEndsWith(String strValue)
 	{
+		LogAction();
 		if(!_driver.getCurrentUrl().endsWith(strValue))
 		{
 			log.error("Current URL does not contain: '" + strValue + "'");
@@ -121,6 +169,7 @@ public class BaseStepUtilsA {
 
 	public static assertContains(String strValue)
 	{
+		LogAction();
 		//assertTrue("Page source did not contain '" + strValue + "'", _driver.getPageSource().contains(strValue));
 
 		if(!_driver.getPageSource().contains(strValue))
@@ -134,6 +183,7 @@ public class BaseStepUtilsA {
 
 	public static assertDoesNotContain(String strValue)
 	{
+		LogAction();
 		//assertTrue("Page source did not contain '" + strValue + "'", _driver.getPageSource().contains(strValue));
 
 		if(_driver.getPageSource().contains(strValue))
